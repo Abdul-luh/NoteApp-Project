2c2e05293889a1431a8af4af80e462bd3103ce05
Abdullah <abdulluhodulate@gmail.com>
2023-01-25 08:59:22 +0100

    Delete event lister rectified

--- main.js
+++ main.js
@@ -1,70 +1,71 @@
 const notesContainer = document.getElementById("app");
 const addNoteButton = notesContainer.querySelector(".add-note");
 
-getNotes().forEach(note => {
-    const noteElement = createNoteElement(note.id, note.content);
-    notesContainer.insertBefore(noteElement, addNoteButton);
+getNotes().forEach((note) => {
+	const noteElement = createNoteElement(note.id, note.content);
+	notesContainer.insertBefore(noteElement, addNoteButton);
 });
 
-addNoteButton.addEventListener("click",() => addNote());
+addNoteButton.addEventListener("click", () => addNote());
 function getNotes() {
-    return JSON.parse(localStorage.getItem("stickynotes-notes") || "[]");
+	return JSON.parse(localStorage.getItem("stickynotes-notes") || "[]");
 }
 
 function saveNotes(notes) {
-    localStorage.setItem("stickynotes-notes", JSON.stringify(notes));
-
-   
+	localStorage.setItem("stickynotes-notes", JSON.stringify(notes));
 }
 
 function createNoteElement(id, content) {
-    const element = document.createElement("textarea");
+	const element = document.createElement("textarea");
 
-    element.classList.add("note");
-    element.value  = content;
-    element.placeholder = "Empty Sticky note"
+	element.classList.add("note");
+	element.value = content;
+	element.placeholder = "Empty Sticky note";
 
-    element.addEventListener("change", () =>{
-        updateNote(id, element.value);
-    });
-    
-    element.addEventListener("dbclick", () =>{
-       const deleteNote = confirm("Are you sure you wish to delete this sticky note?");
-    
-       if (deleteNote) {
-        deleteNote(id, element);
-       }
-    });
-    
-    return element;
+	element.addEventListener("change", () => {
+		updateNote(id, element.value);
+	});
 
+	element.addEventListener("dblclick", () => {
+		const doDelete = confirm(
+			"Are you sure you wish to delete this sticky note?"
+		);
+
+		if (doDelete) {
+			deleteNote(id, element);
+		}
+	});
+
+	return element;
 }
 
 function addNote() {
-    const notes = getNotes();
-    const noteObject = {
-        id: Math.floor(Math.random() * 100000),
-        content: ""
-    };
+	const notes = getNotes();
+	const noteObject = {
+		id: Math.floor(Math.random() * 100000),
+		content: "",
+	};
 
-    const noteElement = createNoteElement(noteObject.id, noteObject.content);
-    notesContainer.insertBefore(noteElement, addNoteButton);
-    
-    notes.push(noteObject);
-    saveNotes(notes);
+	const noteElement = createNoteElement(noteObject.id, noteObject.content);
+	notesContainer.insertBefore(noteElement, addNoteButton);
+
+	notes.push(noteObject);
+	saveNotes(notes);
 }
 
 function updateNote(id, newContent) {
-    const notes = getNotes();
-    const targetNote = notes.filter(note => note.id == id)[0];
+	const notes = getNotes();
+	const targetNote = notes.filter((note) => note.id == id)[0];
 
-    targetNote.content = newContent;
-    saveNotes(notes); 
+	targetNote.content = newContent;
+	saveNotes(notes);
 }
 
 function deleteNote(id, element) {
-    const notes = getNotes().filter(note => note.id != id);
+	console.log("Deleting Note...");
+	const notes = getNotes().filter((note) => note.id != id);
 
-    saveNotes(notes);
-    notesContainer.removeChild(element);
+	saveNotes(notes);
+	notesContainer.removeChild(element);
+	alert("Note Delete!");
 }
